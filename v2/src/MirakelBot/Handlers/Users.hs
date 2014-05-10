5c066326b51a2e4d68d8252b960e7a21d8317971
Richard Pilz <richard.pilz.online@gmx.de>
2014-05-10 19:59:38 +0200

    Fixes user handler

--- v2/src/MirakelBot/Handlers/Users.hs
+++ v2/src/MirakelBot/Handlers/Users.hs
@@ -5,8 +5,18 @@ import           Data.Text               as T
 import           MirakelBot.HandlerHelpers
 import           MirakelBot.Message.Send
 import           MirakelBot.Types
+import           MirakelBot.Handlers
+import Control.Lens
 
 init :: Irc ()
 init = void $ registerBangHandler (T.pack "users") $ \_ -> do
-    users <- getUserList
-    answer (show users)
+    msg <- getMessage
+    let dest = msg ^? privateDestination
+    case dest of
+        Just cs -> forM_ cs $ \c ->
+            case c of
+                ToChannel c' -> do
+                    users <- getUserList c'
+                    answer (T.pack $ show users)
+                _ -> return ()
+        _ -> return ()
